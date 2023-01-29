ee0aa79751bccb368cc29f2e5c4bf491258ba671
fraekfyr91 <laustruplasse@gmail.com>
2023-01-29 22:18:36 +0100

    hi

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ class new_class():
     def __init__(self):
         self.a = 8
         self.b = 2
-    def stuff(self, sentence):
+    def print_something(self, sentence):
         for i in range(self.b, self.a):
             
             print(sentence * i)
